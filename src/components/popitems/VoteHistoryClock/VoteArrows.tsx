8396a6d233132d87a739d785389d7598f4a78c1b
[dyad] <philiplam2010@gmail.com>
2026-01-09 14:28:21 +0800

    [dyad] Verifying feature completeness after refactoring - wrote 3 file(s) + extra files edited outside of Dyad

--- src/components/popitems/VoteHistoryClock/VoteArrows.tsx
+++ src/components/popitems/VoteHistoryClock/VoteArrows.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { getPosition, innerRadius, ringWidth, cx, cy } from './utils';
+import { getPosition, innerRadius, cx, cy } from './utils';
 
 interface VoteArrowsProps {
   arrowData: any[];
@@ -14,10 +14,11 @@ interface VoteArrowsProps {
   pendingNom: any;
   currentDay: number;
   mode: string;
+  ringWidth: number;
 }
 
 const VoteArrows: React.FC<VoteArrowsProps> = ({
-  arrowData, playerCount, playerNo, isVoting, isSliding, gestureStart, gestureCurrent, pendingNom, currentDay, mode
+  arrowData, playerCount, playerNo, isVoting, isSliding, gestureStart, gestureCurrent, pendingNom, currentDay, mode, ringWidth
 }) => {
   const drawArrow = (from: number, to: number, day: number, type: 'to' | 'from' | 'self', width = 2) => {
     const color = type === 'to' ? '#ef4444' : type === 'from' ? '#22c55e' : '#a855f7';
