8d3c5fa309c76c22624b15738dd3bd74437af26b
philiplam <74084724+philiplamscript@users.noreply.github.com>
2026-01-09 21:47:53 +0800

    Clockface displayment

--- src/components/popitems/VoteHistoryClock/VoteHistoryClock.tsx
+++ src/components/popitems/VoteHistoryClock/VoteHistoryClock.tsx
@@ -164,7 +164,7 @@ const VoteHistoryClock: React.FC<VoteHistoryClockProps> = (props) => {
         onTouchMove={(e) => handleMove(e.touches[0].clientX, e.touches[0].clientY)} onTouchEnd={handleEnd}
       >
         <defs>
-          <radialGradient id="playerSpotlight" cx="50%" cy="50%" r="50%">
+          <radialGradient id="playerSpotlight" cx="0%" cy="0%" r="0%">
             <stop offset="0%" stopColor="#fef3c7" stopOpacity="1" />
             <stop offset="50%" stopColor="#fef3c7" stopOpacity="0.8" />
             <stop offset="100%" stopColor="#fde68a" stopOpacity="0.4" />
